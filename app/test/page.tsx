318eff21443cada2a57185da6491641f10572952
SamsonVH <samson.vh.20@gmail.com>
2024-01-13 22:49:24 +0700

    Test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,10 +2,10 @@ import { GetMembershipPackages } from "@/networks/apis/MembershipPackagesAPIs";
 import dynamic from "next/dynamic";
 import React from "react";
 
-const page = async () => {
-  const a = await GetMembershipPackages();
+const page =  () => {
+  const a =  GetMembershipPackages();
 
-  return <div>{a.totalItem}</div>;
+  return <div>{a.then(res => res.totalItem)}</div>;
 };
 
 export default dynamic(() => Promise.resolve(page), { ssr: false });
--- networks/apis/MembershipPackagesAPIs.ts
+++ networks/apis/MembershipPackagesAPIs.ts
@@ -3,5 +3,6 @@ import { TGetMembershipPackages } from "@/data/ResponseTypes";
 export const GetMembershipPackages: TGetMembershipPackages = async () => {
   const url =
     process.env.API_SERVER + process.env.API_VERSION + "/membership-packages";
-  return await fetch(url).then((res) => res.json());
+  const data = await fetch(url).then((res) => res.json());
+  return data;
 };
