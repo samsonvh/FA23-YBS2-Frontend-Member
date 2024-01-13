3b34825ee7f94250203c94b827ec0c3673e0dfe1
SamsonVH <samson.vh.20@gmail.com>
2024-01-13 15:34:30 +0700

    Log

--- networks/apis/ToursAPIs.ts
+++ networks/apis/ToursAPIs.ts
@@ -6,6 +6,8 @@ setGlobalDispatcher(new Agent({ connect: { timeout: 300_000 } }));
 export const getTourById: TGetTourById = async (id: string) => {
   const url = process.env.API_SERVER + process.env.API_VERSION + "/tours/" + id;
   const res = await fetch(url);
+  console.log(res.status )
+  console.log(res)
   if (res) {
     return await res.json();
   }
